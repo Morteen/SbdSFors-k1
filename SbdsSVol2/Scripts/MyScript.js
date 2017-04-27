f83f8c44062aae1865638413e5c12588c69b1fb3
morteen <mortenolsen4@gmail.com>
2017-04-27 09:18:58 +0200

    Laget delete funksjon påIndex/admin siden og la den i script mappen på filen MyScript

---
+++ SbdsSVol2/Scripts/MyScript.js
@@ -0,0 +1,32 @@
+﻿
+
+    $(document).ready(function () {
+
+
+        
+    })
+function ConfirmDelete(AtCourseId) {
+    $("#hiddenApplicationId").val(AtCourseId);
+    $("#deleteModal").modal("show");
+}
+//Sletter brukere ved kurs
+function DeleteApplication() {
+    $("#loaderDiv").show();
+    var CorId = $("#hiddenApplicationId").val();
+
+    $.ajax({
+        type: "POST",
+        url: "/Account/AtCourseDelete",
+        data: { AtCourseId: CorId },
+        success: function (result) {
+            $("#loaderDiv").hide();
+            $("#row_" + CorId).remove();
+            $("#deleteModal").modal('hide');
+        },
+        error: function (xhr, status, error) {
+            alert("En feil oppsto ved sletting - " + error);
+            $("#loaderDiv").hide();
+            $("#deleteModal").modal('hide');
+        }
+    })
+}
